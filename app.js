b2ff6f384b52c961bfa988748f9ed51177045e7f
tamaresserman <tamar.esserman@gmail.com>
2017-02-13 11:31:34 -0800

    m

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var path = require('path');
 var handlebars = require('express3-handlebars')
 
 var index = require('./routes/index');
+var tasks = require('./routes/tasks');
+var hobbies = require('.routes/hobbies');
 // Example route
 // var user = require('./routes/user');
 
