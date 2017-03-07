0d2dab50b8a237aa07d244103e81e000acc711b7
tamaresserman <tamar.esserman@gmail.com>
2017-03-06 17:01:51 -0800

    newpage

---
+++ public/js/tasks.js
@@ -0,0 +1,20 @@
+// Your code goes here
+$(document).ready(function() {
+$('#taskForm').submit(function(e) {
+    // Prevents default submit + reload (we only want the submit part)
+    e.preventDefault();
+
+    var taskName = $('#addTask').val();
+
+    // Send the POST request
+    $.post('addTask', { taskName: taskName }, postCallback);
+  });
+});
+
+function postCallback(res) {
+  $('.taskList').append('
+' + res + '
+'); // Add to the list
+
+ $('#addTask').val(''); // Clear form
+}
